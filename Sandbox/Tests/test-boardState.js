e376a7844a8f783579dc7a4c4dba7f5d78071691
Adam Flynn <flynnbox@gmail.com>
2012-07-18 12:26:26 -0400

    moving forward with boardState tests

--- Sandbox/Tests/test-boardState.js
+++ Sandbox/Tests/test-boardState.js
@@ -1,6 +1,21 @@
 module( "boardState" );
 
-test( "a basic test example", function() {
-	var value = "hello";
-	equal( value, "hello", "We expect value to be hello" );
+var arimaaSpec = null;
+
+function getBoardSpec(){	
+	return arimaaSpec.defineBoard(2, 2, [new Square(1, 1)]);
+}
+
+function setup(){
+	arimaaSpec = new arimaa.specification();
+}
+
+QUnit.moduleStart(setup);
+
+//QUnit.testStart();
+
+test( "construct basic board", function() {
+	var boardState = new arimaa.boardState(getBoardSpec());
+	notStrictEqual( typeof(boardState), 'undefined', "boardState is undefined");
+	notStrictEqual( boardState, null, "boardState is null");
 });
