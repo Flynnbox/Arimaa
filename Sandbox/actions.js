6afc7b5a798d3b92a2d136a9d685f9ccf2e7edd1
Flynnbox <flynnbox@gmail.com>
2012-08-04 11:18:25 -0400

    added action file to handle the lare grained actions on each turn

---
+++ Sandbox/actions.js
@@ -0,0 +1,73 @@
+//take raw input and handle the overall action for a team, whether setup or movement
+//delegate to rulesEngine the validation of the action based on gameState
+//delegate to interactions the notification of the user or collection of input
+
+if (typeof(arimaa) === 'undefined') {
+    alert('requires arimaa.js');
+}
+
+if (typeof(arimaa.canvasRenderer) === 'undefined') {
+    arimaa.renderer = {};
+}
+
+arimaa.action = function(ruleEngine) {
+	"use strict";
+
+	var isSetup = false,
+			currentTeam = 'gold',
+			moveCount = 0,
+			rules = ruleEngine,
+
+	movePiece = function(piece, square){
+		/*if (team !== currentTeam){
+			arimaa.trigger('arimaa.error', 'arimaa::action::movePiece: Current Team is ' + currentTeam + ' but move recieved was from ' + team)
+			return;
+		}*/
+
+		if (isSetup){
+			rules.setPiece(currentTeam, piece, square);
+			arimaa.trigger('arimaa.setup', {"piece": piece, "square": square});
+			return;
+		}
+		if (moveCount === 4){
+			error('An attempt was made to make more than 4 moves.');
+			return;
+		}
+		moveCount++;
+		rules.movePiece(currentTeam, piece, square);
+		arimaa.trigger('arimaa.move', {"piece": piece, "square": square});
+	},
+
+	beginTurn = function(){
+		moveCount = 0;
+		//arimaa.trigger('arimaa.beginTurn', {"currentTeam": currentTeam});
+	},
+
+	endTurn = function(){
+		//TODO: Validate current board state and see if anyone won
+		switchTeam();
+	},
+
+	switchTeam = function(){
+		if(currentTeam === 'gold'){
+			currentTeam = 'silver';
+			return;
+		}
+		currentTeam = 'gold';
+	}
+
+	subscribe = function(){
+		arimaa.on('arimaa.movePiece', movePiece);
+		arimaa.on('arimaa.beginTurn', beginTurn);
+		arimaa.on('arimaa.endTurn', endTurn);
+	},
+
+	error = function(functionName, message){
+		arimaa.error('arimaa::action::' + error.caller + ': ' + message);
+	};
+
+	subscribe();
+
+	return {
+	};
+};
